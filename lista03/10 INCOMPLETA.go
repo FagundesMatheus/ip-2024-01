e87ec3049cf0afdbb185c4ed82072d8407e11fda
FagundesMatheus <166179579+FagundesMatheus@users.noreply.github.com>
2024-05-09 13:02:31 -0300

    Update 10.go

--- lista03/10 INCOMPLETA.go
+++ lista03/10 INCOMPLETA.go
@@ -3,24 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var n, max int
-	fmt.Scanln(&n)
-	freq := make([]int, 11)
-	nums := make([]int, n)
-	for i := 0; i < n; i++ {
-		fmt.Scan(&nums[i])
-		freq[nums[i]] += 1
-		if nums[i] > max {
-			max = i
-		}
-		if i == n-1 {
-			fmt.Printf("nota: %d, %d vezes \n", nums[i], freq[nums[i]])
-		}
-	}
-	for i := 0; i < 11; i++ {
-
-	}
-	fmt.Printf("nota: %d, indice %d \n", max, freq[max])
+    var n, max, maxIndex int
+    fmt.Scanln(&n)
+    freq := make([]int, 11)
+    nums := make([]int, n)
+    for i := 0; i < n; i++ {
+        fmt.Scan(&nums[i])
+        freq[nums[i]]++
+        if nums[i] > max {
+            max = nums[i]
+            maxIndex = i
+        }
+    }
+    fmt.Printf("nota: %d, %d vezes\n", nums[n-1], freq[nums[n-1]])
+    fmt.Printf("nota: %d, indice %d\n", max, maxIndex)
 }
-
-//11 5 6 3 4 3 8 7 4 8 6 4
