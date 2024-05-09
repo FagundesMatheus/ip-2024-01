815fb7fdbba632d0e63215cd2358b3b370a69bcf
FagundesMatheus <166179579+FagundesMatheus@users.noreply.github.com>
2024-05-09 12:41:33 -0300

    Update 07.go

--- lista03/07 INCOMPLETA.go
+++ lista03/07 INCOMPLETA.go
@@ -1,19 +1,30 @@
-
 package main
+
 import "fmt"
 
 func main() {
-  var n, max int
-  fmt.Scanln(&n)
-  nums := make([]int, n)
-  var m map[int]int
-  for i := 0; i < n; i++{
-      fmt.Scan(&nums[i])
-      if nums[i] > max {max = nums[1]}
-      m[nums[i]] += m[nums[1]]
-  }
-  for i := 0; i < max; i++{
-      fmt.Println("(%d) %d", i, m[i])
-  }
- 
+    var n, max int
+    for {
+        fmt.Scanln(&n)
+        if n == 0 {
+            break
+        }
+
+        nums := make([]int, n)
+        for i := 0; i < n; i++ {
+            fmt.Scan(&nums[i])
+            if nums[i] > max {
+                max = nums[i]
+            }
+        }
+
+        m := make(map[int]int)
+        for _, num := range nums {
+            m[num]++
+        }
+
+        for i := 0; i <= max; i++ {
+            fmt.Printf("(%d) %d\n", i, m[i])
+        }
+    }
 }
